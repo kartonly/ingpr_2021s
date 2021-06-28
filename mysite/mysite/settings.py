6cce97d933edfea41398c966e545050599ef4726
kartonly <kartonly@yandex.ru>
2021-06-28 23:33:36 +0300

    final

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rangefilter',
     'rest_framework',
     'import_export',
+    'accounts.apps.AccountsConfig',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = '/'
+
